8e71b3843ebf09a572c3475e697023bb2e0228e5
Probeloff <degtyarev.vg@mail.ru>
2018-10-09 21:29:25 +0700

    section reviews. finish

--- app/blocks.project/section-reviews/section-reviews.js
+++ app/blocks.project/section-reviews/section-reviews.js
@@ -2,6 +2,39 @@
 
 $(function() {
 
-  
+  let reviewsSlider = $('.section-reviews__slider').slick({
+    infinite: true,
+    fade: false,
+    autoplay: false,
+    speed: 300,
+    autoplaySpeed: 5000,
+    pauseOnDotsHover: true,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    swipeToSlide: true,
+    swipe: true,
+    adaptiveHeight: true,
+    dots: true,
+    arrows: true,
+    appendArrows: $('.section-reviews__arrows'),
+    prevArrow: `
+      <span class='slider-arrow slider-arrow_363636 slider-arrow_prev section-reviews__prev'>
+        <span class='icon icon_arrow-363636'></span>
+      </span>`,
+    nextArrow: `
+      <span class='slider-arrow slider-arrow_363636 slider-arrow_next section-reviews__next'>
+        <span class='icon icon_arrow-363636'></span>
+      </span>`,
+    appendDots: $('.section-reviews__dots'),
+    /*responsive: [
+      {
+        breakpoint: 1199.9,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }
+    ]*/
+  });
 
 });
